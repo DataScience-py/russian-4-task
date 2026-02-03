35536859d90fcce0cfc39e25eda200443d9f1e2e
Anton Golubchikov <anton1programmist@gmail.com>
2026-02-03 22:18:58 +0700

    first version

---
+++ src/run_bot.py
@@ -0,0 +1,80 @@
+import os
+import random
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, CommandHandler, ContextTypes
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
+token = os.getenv("TG_TOKEN")
+
+
+stress_symb = "ёеуыаоэяию"
+stress_symb += stress_symb.upper()
+
+with Path.open(BASE_DIR / "words.txt", 'r', encoding="utf-8") as f:
+    lines = [line.strip() for line in f if line.strip()]
+
+
+def get_random_word() -> str:
+    return random.choice(lines)
+
+async def get_user_markup(word:str, context: ContextTypes.DEFAULT_TYPE):
+    words = []
+    word_without_context = word.split("(")[0].strip()
+    for i in range(len(word_without_context)):
+        if word_without_context[i] in stress_symb:
+            words.append(word_without_context[:i].lower() + word_without_context[i].upper() + word_without_context[i+1:].lower())
+
+    keyboard = [
+            [InlineKeyboardButton(i, callback_data=i)] for i in words
+            ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.user_data["word"] = word_without_context
+
+    return reply_markup
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.message is None:
+        return
+    word = get_random_word()
+    reply_markup = await get_user_markup(word, context)
+
+    await update.message.reply_text(f"Слово: {word.upper()}", reply_markup=reply_markup)
+
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+    if context.user_data is None:
+        return
+    word = context.user_data.get("word", "")
+    user_choice = query.data
+
+    text = ""
+    if user_choice == word:
+        text = f"✅ Правильно! {word}\n\n"
+    else:
+        text = f"❌ Неверно, правильно: {word}\n\n"
+    word = get_random_word()
+    reply_markup = await get_user_markup(word, context)
+    text += f"Слово: {word.upper()}\n"
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
+
+
+
+app = ApplicationBuilder().token(token).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(button_handler))
+
+app.run_polling()
+
+
+
+
